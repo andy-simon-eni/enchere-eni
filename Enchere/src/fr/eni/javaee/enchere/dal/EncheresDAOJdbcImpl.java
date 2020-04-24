9eeac3fe21f367818c6e856999267f9e832b5895
Prioux <stevenprioux@gmail.com>
2020-04-24 07:56:56 +0200

    Création class static pour construire les entités BO

--- Enchere/src/fr/eni/javaee/enchere/dal/EncheresDAOJdbcImpl.java
+++ Enchere/src/fr/eni/javaee/enchere/dal/EncheresDAOJdbcImpl.java
@@ -75,8 +75,8 @@ public class EncheresDAOJdbcImpl implements EncheresDAO {
 				Encheres ench = new Encheres();
 				ench.setDate_enchere(rs.getDate("date_enchere").toLocalDate());
 				ench.setMontant_enchere(rs.getInt("montant_enchere"));
-				ench.setNo_utilisateur(getObjectUtil(rs));
-				ench.setNo_article(getObjectArticle(rs));
+				ench.setNo_utilisateur(ObjectBuilder.getObjectUtil(rs));
+				ench.setNo_article(ObjectBuilder.getObjectArticle(rs));
 				listEnch.add(ench);
 			}
 		} catch (SQLException e) {
@@ -100,8 +100,8 @@ public class EncheresDAOJdbcImpl implements EncheresDAO {
 				ench.setDate_enchere(rs.getDate("date_enchere").toLocalDate());
 				ench.setMontant_enchere(rs.getInt("montant_enchere"));
 				listEnch.add(ench);
-				ench.setNo_utilisateur(getObjectUtil(rs));
-				ench.setNo_article(getObjectArticle(rs));
+				ench.setNo_utilisateur(ObjectBuilder.getObjectUtil(rs));
+				ench.setNo_article(ObjectBuilder.getObjectArticle(rs));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -124,8 +124,8 @@ public class EncheresDAOJdbcImpl implements EncheresDAO {
 				ench.setDate_enchere(rs.getDate("date_enchere").toLocalDate());
 				ench.setMontant_enchere(rs.getInt("montant_enchere"));
 				listEnch.add(ench);
-				ench.setNo_utilisateur(getObjectUtil(rs));
-				ench.setNo_article(getObjectArticle(rs));
+				ench.setNo_utilisateur(ObjectBuilder.getObjectUtil(rs));
+				ench.setNo_article(ObjectBuilder.getObjectArticle(rs));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -135,59 +135,7 @@ public class EncheresDAOJdbcImpl implements EncheresDAO {
 		}
 		return listEnch;
 	}
-
-	private Utilisateurs getObjectUtil(ResultSet rs) throws SQLException {
-		Utilisateurs util = new Utilisateurs();
-		util.setNo_utilisateur(rs.getInt("no_utilisateur"));
-		util.setPseudo(rs.getString("pseudo"));
-		util.setNom(rs.getString("nom"));
-		util.setPrenom(rs.getString("prenom"));
-		util.setEmail(rs.getString("email"));
-		util.setTelephone(rs.getString("telephone"));
-		util.setRue(rs.getString("rue"));
-		util.setCode_postal(rs.getString("code_postal"));
-		util.setVille(rs.getString("ville"));
-		util.setMot_de_passe(rs.getString("mot_de_passe"));
-		util.setCredit(rs.getInt("credit"));
-		util.setAdministrateur(rs.getBoolean("administrateur"));
-		return util;
-	}
-
-	private Categories getObjectCategories(ResultSet rs) throws SQLException {
-		Categories cat = new Categories();
-		cat.setNo_categorie(rs.getInt("no_categorie"));
-		cat.setLibelle(rs.getString("libelle"));
-
-		return cat;
-	}
-
-	private Retraits getObjectRetraits(ResultSet rs) throws SQLException {
-		Retraits retrait = new Retraits();
-		retrait.setRue(rs.getString("rue"));
-		retrait.setCode_postal(rs.getString("code_postal"));
-		retrait.setVille(rs.getString("ville"));
-
-		return retrait;
-	}
-
-	private ArticlesVendus getObjectArticle(ResultSet rs) throws SQLException {
-		ArticlesVendus art = new ArticlesVendus();
-		Categories cat = getObjectCategories(rs);
-		Utilisateurs util = getObjectUtil(rs);
-		Retraits retrait = getObjectRetraits(rs);
-		art.setNo_article(rs.getInt("no_article"));
-		art.setNom_article(rs.getString("nom_article"));
-		art.setDescription(rs.getString("description"));
-		art.setDate_debut(rs.getDate("date_debut_encheres").toLocalDate());
-		art.setDate_fin(rs.getDate("date_fin_encheres").toLocalDate());
-		art.setPrix_initial(rs.getInt("prix_initial"));
-		art.setPrix_vente(rs.getInt("prix_vente"));
-		art.setUtil(util);
-		art.setCategorie(cat);
-		art.setRetrait(retrait);
-
-		return art;
-	}
+	
 
 	@Override
 	public Encheres getInfosMaxEnchereByNoArticle(int no_article) throws BusinessException {
@@ -199,8 +147,8 @@ public class EncheresDAOJdbcImpl implements EncheresDAO {
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
 				uneEnchere = new Encheres();
-				Utilisateurs util = this.getObjectUtil(rs);
-				ArticlesVendus article = this.getObjectArticle(rs);
+				Utilisateurs util = ObjectBuilder.getObjectUtil(rs);
+				ArticlesVendus article = ObjectBuilder.getObjectArticle(rs);
 				uneEnchere.setNo_utilisateur(util);
 				uneEnchere.setNo_article(article);
 				uneEnchere.setMontant_enchere(rs.getInt("montant_enchere"));
@@ -278,8 +226,8 @@ public class EncheresDAOJdbcImpl implements EncheresDAO {
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
 				uneEnchere = new Encheres();
-				Utilisateurs util = this.getObjectUtil(rs);
-				ArticlesVendus article = this.getObjectArticle(rs);
+				Utilisateurs util = ObjectBuilder.getObjectUtil(rs);
+				ArticlesVendus article = ObjectBuilder.getObjectArticle(rs);
 				uneEnchere.setNo_utilisateur(util);
 				uneEnchere.setNo_article(article);
 				uneEnchere.setMontant_enchere(rs.getInt("montant_enchere"));
